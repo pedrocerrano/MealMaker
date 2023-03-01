89f540bfedf2a89e8c3fc2f578e802bc3f3422d3
pedrocerrano <82617951+pedrocerrano@users.noreply.github.com>
2023-03-01 10:23:31 -0600

    Adds NetworkError file and begins to build Network Controller.

---
+++ MealMaker/MealMaker/Networking/MealService.swift
@@ -0,0 +1,16 @@
+//
+//  MealService.swift
+//  MealMaker
+//
+//  Created by iMac Pro on 3/1/23.
+//
+
+import Foundation
+
+struct MealService {
+    
+    static func fetchAllCategories(completion: @escaping (Result<[Category], NetworkError>) -> Void) {
+        
+    }
+    
+}
